e6e8bb60c00b1a3811de92f1b6ed41f0cd49a5cc
Lolipop <xlolipopxn@gmail.com>
2026-02-26 16:45:03 +0700

    Add plugin strapi ( Google cloud Storage )

--- projectmanage-web/Backend-cms/config/plugins.ts
+++ projectmanage-web/Backend-cms/config/plugins.ts
@@ -29,4 +29,16 @@ export default ({ env }) => ({
           },
         },
       },
+      
+    //For Google Cloud Storage
+    // upload: {
+    //   config: {
+    //     provider: 'strapi-provider-upload-google-cloud-storage',
+    //     providerOptions: {
+    //       bucketName: env('GCS_BUCKET_NAME'),
+    //       publicFiles: true,
+    //       uniform: true,
+    //     },
+    //   },
+    // },
 });
